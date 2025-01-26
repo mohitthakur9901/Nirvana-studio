4b7009f072154f70da5f0b711568e631515696c7
mohit <mohitthakur9901@gmail.com>
2025-01-26 14:36:50 +0530

    fix  work page issue

--- src/components/WorkPage.tsx
+++ src/components/WorkPage.tsx
@@ -1,25 +1,39 @@
-"use client"
-
+"use client";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import Safari from "./ui/safari";
 
 export default function WorkPage() {
-    const isMobile = useIsMobile();
-    return (
-        <div>
-            <h1 className={`${isMobile ? "text-[6rem] font-semibold font-serif " : "text-[10rem] font-semibold font-serif"}`}>OUR WORK</h1>
+  const isMobile = useIsMobile();
+  return (
+    <div className={isMobile ? "w-full px-4" : "px-10"}>
+      <h1
+        className={`${
+          isMobile ? "text-[4rem]" : "text-[10rem]"
+        } font-semibold font-serif text-center`}
+      >
+        OUR WORK
+      </h1>
 
-            <div className="flex flex-col gap-y-5 items-center justify-center mb-5">
-                <h1 className="font-semibold text-xl underline underline-offset-8">Idea to Reality</h1>
-                <Safari className={`${isMobile ? "h-[10rem] w-[10rem]" : ""}`} imageSrc="https://cdn.prod.website-files.com/6396257fec13a8df1b522d2f/65803ddd29f550aa3a985ca2_Paradigm-Home-Page-Section-Our-Works.webp" />
-                <h1>
+      <div className="flex flex-col gap-y-5 items-center justify-center mb-5">
+        <h1 className="font-semibold text-xl underline underline-offset-8">
+          Idea to Reality
+        </h1>
 
-                </h1>
-                <Safari className={`${isMobile ? "h-[10rem] w-[10rem]" : ""}`} imageSrc="https://cdn.prod.website-files.com/6396257fec13a8df1b522d2f/64e6028c1b4ae39f8d470352_Euphemia%20About%20Page%20Section%20Portfolio-p-1600.webp" />
+        <Safari
+          className={`${
+            isMobile ? "w-full max-w-xs h-auto" : "w-[10rem] h-[10rem]"
+          }`}
+          imageSrc="https://cdn.prod.website-files.com/6396257fec13a8df1b522d2f/65803ddd29f550aa3a985ca2_Paradigm-Home-Page-Section-Our-Works.webp"
+        />
 
-            </div>
-        </div>
-    );
-
-};
+        <Safari
+          className={`${
+            isMobile ? "w-full max-w-xs h-auto" : "w-[10rem] h-[10rem]"
+          }`}
+          imageSrc="https://cdn.prod.website-files.com/6396257fec13a8df1b522d2f/64e6028c1b4ae39f8d470352_Euphemia%20About%20Page%20Section%20Portfolio-p-1600.webp"
+        />
+      </div>
+    </div>
+  );
+}
